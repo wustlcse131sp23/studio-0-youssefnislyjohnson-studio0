56580171efe8b75eedd4f76de5fd36219fae1ff1
pavly-washu <114446084+pavly-washu@users.noreply.github.com>
2023-01-19 12:00:32 -0600

    Hi

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -8,7 +8,7 @@ public class BurritoBudget {
 		int burritosEatenWeek3 = 19; //don't judge me
 		int burritosEatenWeek4 = 1;
 		
-		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
+		double totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
 		double averageWeeklyBurritos = totalBurritos / 4;
 		
 		double burritoPrice = 7.50;
